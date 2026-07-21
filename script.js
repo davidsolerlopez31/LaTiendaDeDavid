66c15eab4b41400815c141513e29f067299a7da1
davidsolerlopez31 <davidsolerlopez31@gmail.com>
2026-07-21 16:59:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -198,3 +198,54 @@ document.querySelectorAll(".detalles").forEach(boton=>{
     });
 
 });
+
+.modal{
+
+    position:fixed;
+    top:0;
+    left:0;
+
+    width:100%;
+    height:100%;
+
+    background:rgba(0,0,0,.7);
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+    z-index:999;
+
+}
+
+.modal-contenido{
+
+    background:white;
+
+    width:90%;
+    max-width:500px;
+
+    padding:20px;
+
+    border-radius:15px;
+
+    text-align:center;
+
+}
+
+.modal img{
+
+    width:100%;
+    border-radius:10px;
+
+}
+
+#cerrarModal{
+
+    float:right;
+
+    font-size:30px;
+
+    cursor:pointer;
+
+}
